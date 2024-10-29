616ea2d3b38d2cc6d2787870afdacc7d7bd63e2d
Piyush Choudhari <choudhari.piyush@gmail.com>
2024-10-29 12:38:39 +0530

    added json parsing

--- MarsDataStream/src/main/java/org/mars/Main.java
+++ MarsDataStream/src/main/java/org/mars/Main.java
@@ -1,11 +1,15 @@
 package org.mars;
 
+import org.json.JSONObject;
+
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         GetAPI getAPI = new GetAPI();
-        String run = getAPI.run("https://api.nasa.gov/insight_weather/?api_key=dphfgZwR6wN9w9i4ktUiswSVc85bPHXD0Ah5GXkd&feedtype=json&ver=1.0");
-        System.out.println(run);
+        String response = getAPI.run("https://api.nasa.gov/insight_weather/?api_key=dphfgZwR6wN9w9i4ktUiswSVc85bPHXD0Ah5GXkd&feedtype=json&ver=1.0");
+        JSONObject responseJson = new JSONObject(response);
+        responseJson.remove("sol_keys");
+        System.out.println(responseJson);
     }
 }
