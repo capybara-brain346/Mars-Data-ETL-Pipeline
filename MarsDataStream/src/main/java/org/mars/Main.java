40feb537c4751c7a0628f6a4ba83c2ebca959ba6
Piyush Choudhari <choudhari.piyush@gmail.com>
2024-10-29 17:34:50 +0530

    fixed security issue

--- MarsDataStream/src/main/java/org/mars/Main.java
+++ MarsDataStream/src/main/java/org/mars/Main.java
@@ -1,5 +1,6 @@
 package org.mars;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -7,10 +8,11 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        Dotenv dotenv = Dotenv.load();
         GetAPI getAPI = new GetAPI();
         MongoDBClient mongoDBClient = new MongoDBClient();
 
-        String response = getAPI.run("https://api.nasa.gov/insight_weather/?api_key=dphfgZwR6wN9w9i4ktUiswSVc85bPHXD0Ah5GXkd&feedtype=json&ver=1.0");
+        String response = getAPI.run("https://api.nasa.gov/insight_weather/?api_key=" + dotenv.get("NASA_API_KEY") + "&feedtype=json&ver=1.0");
         JSONObject responseJson = new JSONObject(response);
         responseJson.remove("sol_keys");
         JSONArray responseArray = new JSONArray();
