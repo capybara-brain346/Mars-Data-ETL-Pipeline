901949354e129367064579e7f49689fc545b545a
Piyush Choudhari <choudhari.piyush@gmail.com>
2024-11-01 13:23:01 +0530

    server setup complete

--- MarsDataStream/src/main/java/org/mars/server/Server.java
+++ MarsDataStream/src/main/java/org/mars/server/Server.java
@@ -1,8 +1,10 @@
-package org.mars;
+package org.mars.server;
 
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Server {
     private final ServerSocket serverSocket;
@@ -11,7 +13,7 @@ public class Server {
         this.serverSocket = serverSocket;
     }
 
-    public void startServer() {
+    private void startServer() {
         try {
             while (!serverSocket.isClosed()) {
                 Socket socket = serverSocket.accept();
@@ -20,19 +22,35 @@ public class Server {
                 BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
 
-                Object[] request = bufferedReader.lines().toArray();
-                System.out.println(request[request.length - 1]);
-                bufferedWriter.write((String) request[request.length - 1]);
-                bufferedWriter.newLine();
-                bufferedWriter.flush();
+                List<String> request = new ArrayList<>();
+                String line;
+                while (!(line = bufferedReader.readLine()).isEmpty()) {
+                    request.add(line);
+                }
 
+                System.out.println("Here" + request);
+
+                String jsonResponse = "{\"message\": \"Hello, Client!\"}";
+                int contentLength = jsonResponse.length();
+
+                String httpResponse = "HTTP/1.1 200 OK\r\n"
+                        + "Content-Type: application/json\r\n"
+                        + "Content-Length: " + contentLength + "\r\n"
+                        + "Connection: close\r\n"
+                        + "\r\n"
+                        + jsonResponse;
+
+
+                bufferedWriter.write(httpResponse);
+                bufferedWriter.flush();
             }
-        } catch (Exception e) {
+        } catch (
+                Exception e) {
             throw new RuntimeException(e);
         }
     }
 
-    public void closeServer() {
+    private void closeServer() {
         try {
             if (this.serverSocket != null) {
                 this.serverSocket.close();
